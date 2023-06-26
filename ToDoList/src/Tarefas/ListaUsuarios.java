5f1997a0ffb84abff26d5748317662b7858aa885
yurirsantos <yuri.santos.cco@gmail.com>
2023-06-26 14:46:31 -0300

    corrections of routes

--- ToDoList/src/Tarefas/ListaUsuarios.java
+++ ToDoList/src/Tarefas/ListaUsuarios.java
@@ -166,7 +166,8 @@ public class ListaUsuarios extends javax.swing.JFrame {
     }//GEN-LAST:event_menuHomeActionPerformed
 
     private void menuCadastroUsuárioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuCadastroUsuárioActionPerformed
-        // TODO add your handling code here:
+        new CadastroUsuario().setVisible(true);
+        setVisible(false);
     }//GEN-LAST:event_menuCadastroUsuárioActionPerformed
 
     /**
